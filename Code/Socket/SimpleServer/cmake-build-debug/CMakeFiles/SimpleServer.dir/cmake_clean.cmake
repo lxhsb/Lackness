9b84470a00763d9d83ec29116d37d4ea0b809789
lxhsb <wyyiori@gmail.com>
2017-03-19 21:54:07 +0800

    第一次用c++写了socket

---
+++ Code/Socket/SimpleServer/cmake-build-debug/CMakeFiles/SimpleServer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimpleServer.dir/main.cpp.o"
+  "SimpleServer.pdb"
+  "SimpleServer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SimpleServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
