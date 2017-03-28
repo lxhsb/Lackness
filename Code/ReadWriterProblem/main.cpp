dbea999ebd5509ee6077bc710839ffd6e0bf275b
lxhsb <wyyiori@gmail.com>
2017-03-28 21:22:26 +0800

    完美的读者写者

--- Code/ReadWriterProblem/main.cpp
+++ Code/ReadWriterProblem/main.cpp
@@ -8,8 +8,9 @@ using namespace std ;
 class RWLock
 {
 private:
-    sem_t mutex ;
-    sem_t db ;
+    sem_t rmutex ;
+    sem_t wmutex;
+    sem_t door ;
     int readerCount ;
 public :
     RWLock() ;
@@ -20,61 +21,73 @@ public :
 };
 RWLock::RWLock() {
     readerCount = 0 ;
-    sem_init(&mutex,0,1);
-    sem_init(&db,0,1);
+    sem_init(&rmutex,0,1);
+    sem_init(&wmutex,0,1);
+    sem_init(&door,0,1);
 
 }
 void RWLock::RLock() {
-    sem_wait(&mutex);
-    readerCount++;
-    if(readerCount == 1)
+    sem_wait(&door);
+    sem_wait(&rmutex);
+    if(readerCount == 0 )
     {
-        sem_wait(&db);
+        sem_wait(&wmutex);
     }
-    sem_post(&mutex);
+    readerCount++ ;
+    sem_post(&rmutex);
+    sem_post(&door);
 }
 void RWLock::RUnLock() {
-    sem_wait(&mutex);
-    readerCount--;
+    sem_wait(&rmutex);
+    readerCount -- ;
     if(readerCount == 0)
     {
-        sem_post(&db);
+        sem_post(&wmutex);
     }
-    sem_post(&mutex);
+    sem_post(&rmutex);
+
 }
 void RWLock::WLock() {
-    sem_wait(&db);
+
+    sem_wait(&door);
+    sem_wait(&wmutex);
 }
 
 void RWLock::WUnLock() {
-    sem_post(&db);
+
+    sem_post(&door);
+    sem_post(&wmutex);
 }
 
 void reader(RWLock *lock,int i)
 {
+
     printf("%d is going to read \n",i);
     lock->RLock();
     printf("%d reading \n",i);
     sleep(2);
-    lock->RUnLock();
     printf("%d read finish  \n",i);
+    lock->RUnLock();
+
 
 }
 void writer(RWLock *lock ,int i )
 {
+
     printf("%d is going to write \n",i);
     lock->WLock();
     printf("%d  writing \n",i);
     sleep(2);
-    lock->WUnLock();
     printf("%d write finish \n",i);
+    lock->WUnLock();
+
 }
 int main() {
     vector<thread*>v ;
     RWLock lock ;
     for(int i = 0 ;i< 100 ;i++)
     {
-        if(i%3 == 0 )
+        if(i%5 == 0 )
         {
             v.push_back(new thread(writer,&lock,i));
         }
